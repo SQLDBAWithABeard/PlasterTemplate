d58359e2b0b6b5b90ea695251e854f4faa3ddd22
Rob Sewell <mrrobsewell@outlook.com>
2017-06-27 13:02:19 +0100

    Import mdule correctly

--- tests/Project.Tests.ps1
+++ tests/Project.Tests.ps1
@@ -1,14 +1,11 @@
 $script:ModuleName = '<%= $PLASTER_PARAM_ModuleName %>'
 # Removes all versions of the module from the session before importing
 Get-Module $ModuleName | Remove-Module
-Import-Module $PSScriptRoot\$ModuleName\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
-# Removes all versions of the module from the session before importing
-Get-Module $ModuleName | Remove-Module
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop
 Describe "PSScriptAnalyzer rule-sets" -Tag Build {
 
     $Rules = Get-ScriptAnalyzerRule
-    $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse | where fullname -notmatch 'classes'
+    $scripts = Get-ChildItem $ModuleBase -Include *.ps1, *.psm1, *.psd1 -Recurse | Where-Object fullname -notmatch 'classes'
 
     foreach ( $Script in $scripts ) 
     {
@@ -27,8 +24,10 @@ Describe "PSScriptAnalyzer rule-sets" -Tag Build {
 
 
 Describe "General project validation: $moduleName" -Tags Build {
-
+    BeforeAll {
+        Get-Module $ModuleName | Remove-Module
+    }
     It "Module '$moduleName' can import cleanly" {
-        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
+        {Import-Module $ModuleBase\$ModuleName.psd1 -force } | Should Not Throw
     }
 }
