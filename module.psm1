d6c74b59997eccb0e04acec26ce3a10ade25948a
Rob Sewell <mrrobsewell@outlook.com>
2017-06-28 14:14:58 +0100

    improved psm1

--- module.psm1
+++ module.psm1
@@ -2,16 +2,34 @@
 param()
 Write-Verbose $PSScriptRoot
 
-# All exported functions - 600ms
-foreach ($function in (Get-ChildItem "$PSScriptRoot\functions\*.ps1")) {
-    $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
-}
+Write-Verbose 'Import everything in sub folders folder'
+
+foreach($folder in @('internal', 'functions'))
+
+{
+
+    $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
+
+    if(Test-Path -Path $root)
+
+    {
+
+        Write-Verbose "processing folder $root"
+
+        $files = Get-ChildItem -Path $root -Filter *.ps1 -Recurse
 
 
-# All internal functions privately available within the toolset - 221ms
-foreach ($function in (Get-ChildItem "$PSScriptRoot\internal\*.ps1")) {
-    $ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
+
+        # dot source each file
+
+        $files | where-Object{ $_.name -NotLike '*.Tests.ps1'} | 
+
+            ForEach-Object{Write-Verbose $_.basename; . $_.FullName}
+
+    }
+
 }
+
 
 
 Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\functions\*.ps1").basename
