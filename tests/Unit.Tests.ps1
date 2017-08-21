8af7e922c5b7a12b5ec9b727ee20b7b996553fa6
Rob Sewell <mrrobsewell@outlook.com>
2017-08-21 12:37:35 +0100

    added env variable for unti tests for refernece for mock artifacts

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -6,7 +6,8 @@ $ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
 if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
     $ModuleBase = Split-Path $ModuleBase -Parent
 }
-
+## this variable is for the VSTS tasks and is to be used for refernecing any mock artifacts
+$Env:ModuleBase = $ModuleBase
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
 Describe "Basic function unit tests" -Tags Build , Unit{
 
